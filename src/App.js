c96a19190164509888a53a8075dd2cebf13d737b
Beattris <beatriceebirim@gmail.com>
2022-08-09 23:22:14 +0100

    mapped the categories

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-
-
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    }
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/*<img src="" alt="" />*/}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img src="" alt="" />*/}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img src="" alt="" />*/}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img src="" alt="" />*/}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img src="" alt="" />*/}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map((category) => {
+       return(
+         <div className="category-container">
+           <div className="background-image" />
+           <div id={category.id} className="category-body-container">
+             <h2>{category.title}</h2>
+             <p>Shop Now</p>
+           </div>
+         </div>
+       ) 
+      })}
     </div>
   );
-}
+};
 
 export default App;
