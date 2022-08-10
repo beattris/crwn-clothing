7aaa9918b47c978db4005ff74359d056f833cb7e
Beattris <beatriceebirim@gmail.com>
2022-08-11 00:22:24 +0100

    set the navigation menu across all pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component.jsx";
+import Navigation from "./routes/navigation/navigation.component.jsx";
+
+const Shop = () => {
+  return <h1>I am the shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
